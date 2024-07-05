91dace2a1904ce1f7c25075e3a1a6e8f1f854391
unknown <yurirui656@example.com>
2024-07-05 11:55:00 +0200

    Usando dados Dinamicos no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+function Calc(a , b) {
+
+  return a + b
+ 
+}
 
 function App() {
+  const title = " Hello, World"
+  const subject = "Me livrei da Maldicao"
+
   return (
     <div>
-      <h1>
-        Hello, World!
-      </h1>
+      <h1>{title}</h1>
+      <p>{subject}</p>
+      <h2>React it's easy like 1 + 1 is {1 + 1}</h2>
+      <span>A soma dos numeros é igual a: {Calc(5, 4)}</span>
     </div>
   )
 }

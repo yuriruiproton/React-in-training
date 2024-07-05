05e155a6b4d71625ee5862d2726019dff9dc2894
unknown <yurirui656@example.com>
2024-07-05 11:12:42 +0200

    Enviando o projecto para Deploy (Conhecendo o Vite)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello, World!
+      </h1>
+    </div>
+  )
+}
+
+export default App
